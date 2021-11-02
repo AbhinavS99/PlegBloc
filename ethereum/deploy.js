f67da139692286ab8af96e975fc64f890555a828
Abhinav Sharma <73695032+AbhinavS99@users.noreply.github.com>
2021-11-02 18:32:33 +0530

    Create deploy.js

---
+++ ethereum/deploy.js
@@ -0,0 +1,30 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require('web3');
+require('dotenv').config();
+const compiledFactory = require('./build/campaignFactory.json')
+const fs = require('fs-extra');
+const path = require('path');
+
+const provider = new HDWalletProvider(
+  process.env.mnemonic,
+  'https://rinkeby.infura.io/v3/cff505b264bb479b9df907c80ee66f5f'
+);
+
+const web3 = new Web3(provider);
+
+const deployedAddressPath = path.resolve(__dirname, '../address.js');
+
+(async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: '0x' + compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log(result.options.address);
+  fs.removeSync(deployedAddressPath);
+  fs.writeFileSync(deployedAddressPath , `module.exports = "${result.options.address}";`);
+  console.log('successfully deployed, contract address now accessible in address.js');
+
+})();
