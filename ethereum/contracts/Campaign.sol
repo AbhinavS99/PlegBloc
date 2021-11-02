54464162a17630f581d6bc477cc76b309039b2cc
Abhinav Sharma <73695032+AbhinavS99@users.noreply.github.com>
2021-11-02 19:12:45 +0530

    Update Campaign.sol

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,22 +1,43 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.7.5;
-    
+
+/**
+ * @title CampaignFactory
+ * @dev CampaignFactory for creating campaign instances
+ */
 contract CampaignFactory{
     address[] public deployed_campaigns;
     
+    /**
+     * @dev Create new campaigns by providing minimum contribution value
+     * @param min_c Minimum contribution to be made to start a campaign
+     */
     function create_campaign(uint min_c) public{
         address new_campaign = address(new Campaign(min_c, msg.sender));
         deployed_campaigns.push(new_campaign);
     }
     
+    /**
+     * @dev Returns a list of campaigns that have been created by the message sender
+     * @return list of addresses of campaigns
+     */
     function getDeployedCampaigns() public view returns(address[] memory){
         return deployed_campaigns;
     }
 }
 
 
+/**
+ * @title Campaign
+ * @dev Campaign smart contract
+ */
 contract Campaign{
+    
+    /**
+     * @title Request
+     * @dev The Request Struct has all the parameters that define a request created by campaign manager/creator
+     */
     struct Request{
         string description;
         uint value;
@@ -32,13 +53,18 @@ contract Campaign{
     uint backers_count; 
     Request[] public requests;
     
-    
+    /* @dev modifier to check if the caller is the campaign creator/manager
+    */
     modifier restrict_to_creator(){
         require(msg.sender == creator);
         _;
     }
     
-    
+    /**
+     * @dev Constrcutor of the campaign
+     * @param min_c minimum contribution to start the campaign
+     * @param manager the address of the campaign creator/manager
+     */
     constructor (uint min_c, address manager) {
         creator = manager;
         minimum_contribution = min_c;
@@ -46,6 +72,9 @@ contract Campaign{
         
     }
     
+    /**
+     * @dev contribute to a campaign (payable)
+     */
     function contribute() public payable {
         require(msg.value >= minimum_contribution);
         backers[msg.sender] = true;
@@ -53,6 +82,12 @@ contract Campaign{
         
     }
     
+    /**
+     * @dev create a new_request restricted to creator
+     * @param description the description telling the requirement and goals of the request
+     * @param value the amount of money requested
+     * @param recepient the recepient of the amount
+     */
     function new_request( string memory description, uint value, address  recepient) 
     public payable restrict_to_creator {
         
@@ -65,6 +100,10 @@ contract Campaign{
         
     }
     
+    /**
+     * @dev approve a request at index i
+     * @param i the index at which the request is to be approved
+     */
     function approve_request(uint i) public {
         //i : request index in array
         Request storage curr_request = requests[i];
@@ -75,6 +114,10 @@ contract Campaign{
         curr_request.yes_count++;
     }
     
+    /**
+     * @dev finalize the transaction after getting the required approvals restricted to creator/manager
+     * @param i the index at which request is to be finalized
+     */
     function make_transaction( uint i) public restrict_to_creator {
         Request storage curr_request = requests[i];
         
