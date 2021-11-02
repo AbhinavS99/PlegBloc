5477d3e056cf281290f0039676794071b0416edb
Abhinav Sharma <73695032+AbhinavS99@users.noreply.github.com>
2021-11-02 18:25:14 +0530

    Create compile.js

---
+++ ethereum/compile.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const solc = require("solc");
+const fs = require("fs-extra");
+
+const build_path = path.resolve(__dirname, "build");
+fs.removeSync(build_path);
+
+const campaign_path = path.resolve(__dirname, "contracts", "Campaign.sol");
+const source = fs.readFileSync(campaign_path, "utf-8");
+const file_name = "Campaign.sol";
+
+var input = {
+  language: "Solidity",
+  sources: {
+    "Campaign.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+fs.ensureDirSync(build_path);
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+if (output.errors) {
+  output.errors.forEach((err) => {
+    console.log(err.formattedMessage);
+  });
+} else {
+  const contracts = output.contracts[file_name];
+  for (let contractName in contracts) {
+    const contract = contracts[contractName];
+    fs.writeFileSync(
+      path.resolve(build_path, `${contractName}.json`),
+      JSON.stringify(contract.abi, null, 2),
+      "utf8"
+    );
+  }
+}
