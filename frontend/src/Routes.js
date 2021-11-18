b4faa12165d36028e06de54a25f275846ab63e1f
Abhinav Sharma <abhiRock99@.github.com>
2021-11-18 09:21:37 +0530

    created-structure

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,18 +1,15 @@
 import React from "react";
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./landingPage/base";
 
-const Routes = () => {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path = "/" exact component = {}/>
-                <Route path = "/signup" exact component = {}/>
-                <Route path = "/signin" exact component = {}/>
-                <PrivateRoute path = "/user/dashboard" exact component ={}/>
-                <PrivateRoute path = "/user/dashboard/mycampaigns" exact component = {}/>
-                <PrivateRoute path = "/user/dashboard/createcampaign" exact component = {}/>
-                <PrivateRoute path = "/user/campaign" exact component = {}/>
-            </Switch>
-        </BrowserRouter>
-    )
-}
+const Router_Main = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router_Main;
---
+++ frontend/src/landingPage/base.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <img src="Logo.svg" alt="" />
+            PlegBloc
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Home;
