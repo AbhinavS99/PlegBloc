75f32de7e4a67001c7ccbe75d3f84e50333d5258
Abhinav Sharma <abhiRock99@.github.com>
2021-11-18 10:03:10 +0530

    commit landing page

--- frontend/src/landingPage/base.js
+++ frontend/src/landingPage/base.js
@@ -1,16 +1,40 @@
 import React from "react";
-
+import Signin from "./signin";
 const Home = () => {
   return (
     <div>
-      <nav className="navbar navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            <img src="Logo.svg" alt="" />
-            PlegBloc
-          </a>
+      <section class="text-gray-600 body-font">
+        <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+          <img
+            class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
+            alt="hero"
+            src="https://raw.githubusercontent.com/AbhinavS99/PlegBloc/main/images/Logo.png"
+          />
+          <div class="text-center lg:w-2/3 w-full">
+            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+              PlegBloc - Trustful Crowdfunding
+            </h1>
+            <p class="mb-8 leading-relaxed">
+              PlegBloc is a trustful crowdfunding platform i.e. it enables the
+              funding partners to know and check as to where and how their
+              funding is used or is proposed to be used by the venture. PlegBloc
+              upholds the principle of trustful crowdfunding by enabling the
+              investors to decide the place where the capital can be spent.
+            </p>
+            <div class="flex justify-center">
+              <button
+                class="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg"
+                onClick={<Signin />}
+              >
+                Sign In
+              </button>
+              <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                Sign Up
+              </button>
+            </div>
+          </div>
         </div>
-      </nav>
+      </section>
     </div>
   );
 };
---
+++ frontend/src/landingPage/signin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Signin() {
+  return (
+    <div>
+      <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        ...
+      </div>
+    </div>
+  );
+}
