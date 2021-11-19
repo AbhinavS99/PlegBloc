5c261c0865aaf1379b8867ef45acb8552e9fa64e
Aditya Singh Rathore <aditya18007@iiitd.ac.in>
2021-11-19 16:21:49 +0530

    About us page created. (This commit is template for adding page)

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./landingPage/base";
+import AboutUs from "./about_us/about_us";
 
 const Router_Main = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
       </Routes>
     </BrowserRouter>
   );
