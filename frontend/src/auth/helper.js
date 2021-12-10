d868714d3d9439a91cfecd66f2fa2af77894130e
Abhinav Sharma <abhiRock99@.github.com>
2021-12-10 12:51:09 +0530

    removed .next

--- frontend/src/auth/helper.js
+++ frontend/src/auth/helper.js
@@ -1,20 +1,18 @@
-import Cookies from 'js-cookie';
-import jwt from 'jsonwebtoken';
+import Cookies from "js-cookie";
+import jwt from "jsonwebtoken";
 
 function isAuthenticated() {
-  const token  = Cookies.get('token');
-  // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhIjoiYSIsImlhdCI6MTYzOTA0MTg4NH0.OSlmfkLnrBNp4DPKIIMYEjRbBpoJuBoPfnI_bugHq_k';
+  const token = Cookies.get("token");
   try {
-    const payload = jwt.verify(token, "bc976108effc39c89e17727f269714a60e8046d9");
+    const payload = jwt.verify(
+      token,
+      "bc976108effc39c89e17727f269714a60e8046d9"
+    );
     const email = payload.email;
     return true;
-  }
-  catch {
+  } catch {
     return false;
   }
-};
+}
 
-
-export {
-  isAuthenticated,
-};
+export { isAuthenticated };
