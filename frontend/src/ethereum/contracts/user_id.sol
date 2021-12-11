501612f249e5223d5c5a2b5f3fe988f0cb1dd4fe
Aditya Singh Rathore <aditya18007@iiitd.ac.in>
2021-12-11 18:54:28 +0530

    Updated Contract

--- frontend/src/ethereum/contracts/user_id.sol
+++ frontend/src/ethereum/contracts/user_id.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.9;
 contract User {
     mapping(string => bytes32) private U_Password;
     mapping(string => string) private U_ipfs;
-    mapping(string => int256) private U_login_count;
 
     function register(
         string memory username,
@@ -13,11 +12,10 @@ contract User {
     ) public  {
         U_Password[username] = keccak256( bytes(password) );
         U_ipfs[username] = ipfs;
-        U_login_count[username] = 0;
     }
 
     function get_ipfs(string memory username, string memory password)
-        public
+        public 
         view
         returns (string memory ipfs)
     {
@@ -30,14 +28,14 @@ contract User {
     }
 
     function login(string memory username, string memory password)
-        public
+        public 
+        view
         returns (int256 count)
     {
         bytes32 actual = U_Password[username];
         bytes32 curr = keccak256( bytes(password) );
         if (actual == curr) {
-            U_login_count[username] += 1;
-            return U_login_count[username];
+            return 1;
         }
         return -1;
     }
